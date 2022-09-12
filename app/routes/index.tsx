d6ee7c01dd5cd73e785cd2339b65bbcbbca308e1
Alex Neep <neepalex@gmail.com>
2022-09-12 09:25:05 +0100

    Add tailwind setup

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,15 @@
+import { useLoaderData } from "@remix-run/react";
+import { db } from "~/utils/firebase.server";
+
+// export const loader = async () => {
+//   // const snapshot = await db.collection("users").get();
+//   // console.log(snapshot);
+// };
+
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div>
+      <h1 className=" font-bold  text-blue-600">Welcome to Remix</h1>
     </div>
   );
 }
