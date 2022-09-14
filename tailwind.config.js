2d58db6a838723a4df6d1214bc3a738f260e8952
Alex Neep <alexander@thesolesupplier.co.uk>
2022-09-14 09:12:34 +0100

    Add custom colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,30 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: ["./app/**/*.{ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          100: "#A2D2FF",
+          300: "#67B6FF",
+          600: "#006CD1",
+          900: "#002E5A",
+        },
+        secondary: {
+          100: "#FFAFCC",
+          300: "#FF70A5",
+          600: "#F5005A",
+          900: "#B80043",
+        },
+        accent: {
+          100: "#CDB4DB",
+          300: "#B28DC8",
+          600: "#9967B6",
+          900: "#5D3773",
+        },
+      },
+    },
   },
   plugins: [],
 };
